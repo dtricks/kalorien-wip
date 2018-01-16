4f47c118dbbb1b14c2f9d41bc66b50fcdb8bdf9f
C4RL0zZo <32815292+C4RL0zZo@users.noreply.github.com>
2018-01-16 23:45:23 +0100

    oli
    hjgukf

---
+++ oli/app/src/main/java/com/fhdw/wip/wip/views/activity_new_entry.java
@@ -0,0 +1,75 @@
+package com.fhdw.wip.wip.views;
+
+import android.app.ActionBar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Spinner;
+
+import com.fhdw.wip.wip.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.util.Collections.addAll;
+
+public class activity_new_entry extends AppCompatActivity {
+
+    Spinner foodlist;
+    ArrayAdapter<String> adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_entry);
+
+        foodlist = (Spinner)findViewById(R.id.unit);
+        ArrayList<String> foodArray = new ArrayList<String>();
+        foodArray.addAll(Arrays.asList(getResources().getStringArray(R.array.myfood)));
+
+        adapter = new ArrayAdapter<String>(
+                activity_new_entry.this,
+                android.R.layout.simple_list_item_1,
+                foodArray
+        );
+        foodlist.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+        MenuItem item = menu.findItem(R.id.list);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.searchbar));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                                              @Override
+                                              public boolean onQueryTextSubmit(String query) {
+                                                  return false;
+                                              }
+
+                                              @Override
+                                              public boolean onQueryTextChange(String newtext) {
+                                                  adapter.getFilter().filter(newtext);
+                                                  return false;
+
+
+
+                                              }
+
+
+                                          }
+        );
+        return super.onCreateOptionsMenu(menu);
+    }
+}
+
